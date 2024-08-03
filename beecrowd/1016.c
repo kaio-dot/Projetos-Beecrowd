20d538a4ee8a591256573d48cf3a494a9560cc38
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-08-03 10:24:46 -0300

    Create 1016.c
    Calcula distância

---
+++ beecrowd/1016.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+int main() {
+    float distancia, tempoY;
+    scanf("%f", &distancia);
+    tempoY = distancia * 2;
+    printf("%.0f minutos\n", tempoY);
+ 
+ 
+    return 0;
+}
