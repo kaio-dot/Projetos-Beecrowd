d78da5bee43eea4c9f8f9584bc7b10fbc4b29f0c
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-08-03 10:25:40 -0300

    Create 1015.c
    Calcula distância entre dois pontos

---
+++ beecrowd/1015.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+     double distancia;
+     double x1, y1, x2, y2;
+
+     scanf("%lf %lf", &x1, &y1);
+     scanf("%lf %lf", &x2, &y2);
+
+     distancia = sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)));
+
+     printf("%.4lf\n", distancia);
+     return 0;
+}
