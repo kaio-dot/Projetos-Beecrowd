8a1e9a6041da191f23963a2d3d82b61c69661c5a
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-08-03 10:27:03 -0300

    Create 1013.c
    Calcula o maior entre dois números

---
+++ beecrowd/1013.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ int maiorAB(int A, int B, int C)
+ {
+     int maxab, max;
+     maxab = (A +B + abs(A - B))/2;
+     max = (maxab + C + abs(maxab - C))/2;
+     
+     return max;
+ }
+int main() {
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    
+    printf("%d eh o maior\n",maiorAB(A, B, C));
+
+    return 0;
+}
