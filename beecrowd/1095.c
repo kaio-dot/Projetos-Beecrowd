28c919d8e6ec163486036ba2caf4c77adfe5ae0e
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-08-07 15:37:06 -0300

    Create 1095.c
    Apenas a saída

---
+++ beecrowd/1095.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int j=60, i=1;
+    while(j>=0){
+        if(j>=0){
+            printf("I=%d ",i);
+            i=i+3;
+            printf("J=%d\n", j);
+            j=j-5;
+
+        }
+    }
+    return 0;
+}
