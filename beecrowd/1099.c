3546db74e1691b71059dde0142085e15b1f6967e
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-08-08 15:35:02 -0300

    Create 1099.c
    Somar os números ímpares entre um intervalo escolhido pelo usuário

---
+++ beecrowd/1099.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int N, X, Y, i, soma;
+
+    scanf("%d", &N);
+
+    while (N--) {
+        scanf("%d %d", &X, &Y);
+
+        soma = 0;
+
+        if (X > Y) {
+            int temp = X;
+            X = Y;
+            Y = temp;
+        }
+
+        for (i = X + 1; i < Y; i++) {
+            if (i % 2 != 0) {
+                soma += i;
+            }
+        }
+
+        printf("%d\n", soma);
+    }
+
+    return 0;
+}
