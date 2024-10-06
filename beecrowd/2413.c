d8d121f5a39407a578558690bd51691ded9b9eeb
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-10-05 21:01:31 -0300

    Create 2413.c
    Easy

---
+++ beecrowd/2413.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num;
+    int primeiraLinha;
+    
+    scanf("%d", &num);
+    
+    int segundaLinha = num*2;
+        
+    primeiraLinha = segundaLinha*2;
+    
+    printf("%d\n", primeiraLinha);
+ 
+    return 0;
+}
